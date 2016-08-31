cc5764afcb6da5a975da3178ac294b694deb09e2
Ahmed-Te7a <ahmedabdelftah95165@gmail.com>
2016-08-31 17:35:48 +0200

    Remove behaviour function from companyController as it's done by checking the user permissions

--- backend/modules/crud/controllers/CompanyController.php
+++ backend/modules/crud/controllers/CompanyController.php
@@ -14,23 +14,5 @@ use yii\filters\AccessControl;
  */
 class CompanyController extends \backend\modules\crud\controllers\base\CompanyController
 {
-    /**
-     *
-     * @inheritdoc
-     * @return array
-     */
-    public function behaviors() {
-        return [
-            'access' => [
-                'class' => AccessControl::className(),
-                'rules' => [
-                    [
-                        'allow' => true,
-                        'roles' => ['admin'],
-                    ],
 
-                ]
-            ]
-        ];
-    }
 }
