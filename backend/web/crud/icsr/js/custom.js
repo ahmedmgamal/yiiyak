3d44d44b6c51939e955010ed2a51027c4c79ff36
Ahmed-Te7a <ahmedabdelftah95165@gmail.com>
2016-09-06 19:41:12 +0200

    Fix dates in date picker

--- backend/web/crud/icsr/js/custom.js
+++ backend/web/crud/icsr/js/custom.js
@@ -19,27 +19,34 @@
 
 function changeDates ()
 {
-    var patient_age = parseInt($("#icsr-patient_age").val());
-    var patient_age_unit = $("#icsr-patient_age_unit option:selected").text();
-    var now =new Date();
+      var patient_age = parseInt($("#icsr-patient_age").val());
+      var patient_age_unit = $("#icsr-patient_age_unit option:selected").text();
+      var now = new Date();
 
     if (patient_age_unit == "Day")
     {
+
         now.setDate(now.getDate() - patient_age);
-        $("#icsr-patient_birth_date").datepicker("setDate",now);
+
         $("#icsr-patient_birth_date").datepicker('option',{ minDate: now , maxDate: new Date()});
+        $("#icsr-patient_birth_date").datepicker("setDate",now);
     }
 
     else if (patient_age_unit == "Month")
     {
+
         now.setMonth(now.getMonth() - patient_age);
+
+       $("#icsr-patient_birth_date").datepicker('option',{ minDate: now , maxDate: new Date()});
         $("#icsr-patient_birth_date").datepicker("setDate",now);
-        $("#icsr-patient_birth_date").datepicker('option',{ minDate: now , maxDate: new Date()});
     }
     else
     {
+
         now.setFullYear(now.getFullYear() - patient_age);
-        $("#icsr-patient_birth_date").datepicker("setDate",now);
+
         $("#icsr-patient_birth_date").datepicker('option',{ minDate: now , maxDate: new Date()});
+        $("#icsr-patient_birth_date").datepicker("setDate",now );
+
     }
 }
