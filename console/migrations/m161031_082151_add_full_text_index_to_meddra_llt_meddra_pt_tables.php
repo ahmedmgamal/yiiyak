69128621a0aaeeaa966fb2244aff855ee8447653
Ahmed-Te7a <ahmedabdelftah95165@gmail.com>
2016-11-22 14:02:22 +0200

    Remove full text search

--- console/migrations/m161031_082151_add_full_text_index_to_meddra_llt_meddra_pt_tables.php
+++ console/migrations/m161031_082151_add_full_text_index_to_meddra_llt_meddra_pt_tables.php
@@ -6,11 +6,11 @@ class m161031_082151_add_full_text_index_to_meddra_llt_meddra_pt_tables extends 
 {
     public function up()
     {
-        $this->execute("ALTER TABLE meddra_llt ADD FULLTEXT (term)");
-        $this->execute("ALTER TABLE meddra_pt ADD FULLTEXT (term)");
-        $this->execute("ALTER TABLE meddra_hlt ADD FULLTEXT (term)");
-        $this->execute("ALTER TABLE meddra_hlgt ADD FULLTEXT (term)");
-        $this->execute("ALTER TABLE meddra_soc ADD FULLTEXT (term)");
+//        $this->execute("ALTER TABLE meddra_llt ADD FULLTEXT (term)");
+//        $this->execute("ALTER TABLE meddra_pt ADD FULLTEXT (term)");
+//        $this->execute("ALTER TABLE meddra_hlt ADD FULLTEXT (term)");
+//        $this->execute("ALTER TABLE meddra_hlgt ADD FULLTEXT (term)");
+//        $this->execute("ALTER TABLE meddra_soc ADD FULLTEXT (term)");
 
 
     }
