d4eedce9519fba972e10ec4f015cdb6605b1d3b3
Te7a <ahmedabdelftah95165@gmail.com>
2016-12-05 12:56:03 +0200

    Add model - migrations for rmp

---
+++ backend/modules/crud/models/Rmp.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace backend\modules\crud\models;
+
+use Yii;
+use \backend\modules\crud\models\base\Rmp as BaseRmp;
+
+/**
+ * This is the model class for table "rmp".
+ */
+class Rmp extends BaseRmp
+{
+}
