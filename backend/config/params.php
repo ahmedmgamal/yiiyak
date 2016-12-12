9387d4c729e0363f1e6ab5de727653b0d72b034a
Te7a <ahmedabdelftah95165@gmail.com>
2016-12-12 18:05:01 +0200

    Fix conflict2

--- backend/config/params.php
+++ backend/config/params.php
@@ -2,8 +2,8 @@
 return [
     'adminEmail' => 'admin@example.com',
     'gaId' => '\'UA-83150001-1\'',
-    'ich' => 'http://estri.ich.org/e2br22/index.htm',
-    'dtd' => 'http://estri.ich.org/e2br22/ich-icsrack-v1_1_dtd.zip',
+
+
     'e2bLkp' => [
         'ichicsrmessageheader' => ['title' => 'ICH ICSR Message Header' , 'element' => 'M.1'],
         'safetyreport' => ['title' => 'Identification of the case safety report' , 'element' => 'A.1'],
