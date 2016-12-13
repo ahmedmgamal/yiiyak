f846622d7fcd63ff821b8ac1219897d6d8ffd6b2
mhosny <mahmoud.e.hosny84@gmail.com>
2016-12-13 10:17:24 +0200

    add Reports link - add summary tabulation link

--- backend/modules/crud/views/drug/index.php
+++ backend/modules/crud/views/drug/index.php
@@ -37,7 +37,6 @@ $this->params['breadcrumbs'][] = $this->title;
     <div class="clearfix crud-navigation">
         <div class="pull-left">
             <?php echo Html::a('<span class="glyphicon glyphicon-plus"></span> ' . Yii::t('app', 'New'), ['create'], ['class' => 'btn btn-success']) ?>
-            <?php echo Html::a('<span class="glyphicon glyphicon-list"></span> ' . Yii::t('app', 'summary tabulations'), ['summary-tabulation'], ['class' => 'btn btn-default']) ?>
         </div>
 
      </div>
