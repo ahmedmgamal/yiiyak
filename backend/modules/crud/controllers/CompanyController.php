e31a64fa78fd50a9b95cf8c87aea36aae2d65512
Te7a <ahmedabdelftah95165@gmail.com>
2016-12-14 23:32:18 +0200

    Add company statistics for drug with icsrs

--- backend/modules/crud/controllers/CompanyController.php
+++ backend/modules/crud/controllers/CompanyController.php
@@ -7,6 +7,7 @@
 
 
 namespace backend\modules\crud\controllers;
+use Yii;
 use yii\filters\AccessControl;
 
 /**
@@ -26,11 +27,91 @@ class CompanyController extends \backend\modules\crud\controllers\base\CompanyCo
                 'rules' => [
                     [
                         'allow' => true,
+                        'actions' => ['statistics'],
+                        'roles' => ['normalUser'],
+                    ],
+                    [
+                        'allow' => true,
                         'roles' => ['admin'],
                     ],
 
                 ]
             ]
         ];
+    }
+
+    public function actionStatistics ()
+    {
+        if (!isset(\Yii::$app->user->identity->id))
+        {
+            Yii::$app->session->setFlash('error',Yii::t('app','Login First To See Your Company Statistics'));
+
+            return $this->redirect(Yii::$app->request->referrer);
+        }
+
+        $currentUserCompany = \Yii::$app->user->identity->company;
+
+       $companyDrugs = $currentUserCompany->getDrugs()->with([
+
+                    'icsrs' => function ($query) {
+                        $query->with(['icsrEvents']);
+                    }])->all();
+
+        $drugsWithIcsrs = [];
+        foreach ($companyDrugs as $key => $obj)
+        {
+            $formattedDrug = $this->formatDrug($obj);
+            $drugsWithIcsrs [] = $formattedDrug['drugsWithIcsrs'];
+        }
+
+
+        return $this->render('statistics',['drugsWithIcsrs' => $drugsWithIcsrs]);
+    }
+
+
+    private function formatDrug ($drugObj)
+    {
+
+        $drugsWithIcsrs = ['name' => $drugObj->trade_name , 'y' => count($drugObj->icsrs)];
+
+        // Ex ['Aglcouma' => ['name' => 'Aglcouma' , 'y' => 50]]
+        $meddraLltWithIcsrs = [];
+        //Ex ['Aglcouma' =>['icsr_id' => 5]]
+        $tempLltArr = [];
+        foreach ($drugObj->icsrs as $key1 => $icsrObj)
+        {
+
+            foreach ($icsrObj->icsrEvents as $key2 => $icsrEventObj)
+            {
+                if(isset($tempLltArr[$icsrEventObj->meddra_llt_text]['icsr_id'][$icsrObj->id]))
+                {
+
+                }
+                else
+                 {
+                     $tempLltArr[] = [$icsrEventObj->meddra_llt_text => ['icsr_id' => $icsrObj->id]];
+                    if (isset($meddraLltWithIcsrs[$icsrEventObj->meddra_llt_text]))
+                    {
+                        $meddraLltWithIcsrs[$icsrEventObj->meddra_llt_text]['y'] += 1;
+                    }
+
+                    else
+                        {
+                            $meddraLltWithIcsrs [$icsrEventObj->meddra_llt_text] = ['name' => $icsrEventObj->meddra_llt_text , 'y' => 1];
+                        }
+                 }
+
+
+            }
+
+
+
+
+        }
+
+//        print_r($meddraLltWithIcsrs);
+//        die();
+        return ['drugsWithIcsrs' => $drugsWithIcsrs];
+
     }
 }
---
+++ backend/modules/crud/views/company/statistics.php
@@ -0,0 +1,31 @@
+<?php
+use miloschuman\highcharts\Highcharts;
+
+
+echo Highcharts::widget([
+    'options' => [
+        'chart' =>[
+            'type' => 'pie'
+        ],
+        'title' => ['text' => Yii::t('app','Icsrs In Drugs')],
+
+        'plotOptions' => [
+
+            'pie' => [
+                'dataLabels' => [
+                    'enabled' => true,
+                    'format' => '<b>{point.name}</b>: {point.percentage:.1f} %'
+                ]
+            ]
+        ],
+        'series' => [
+            ['name' => 'Icsrs',
+                'data' => $drugsWithIcsrs
+
+
+            ],
+
+        ]
+    ]
+]);
+
