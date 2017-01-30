7a171b25a266ec5b804e5f3bbe9275ce1658c0f3
Mahmooud Hosny <mahmoud.e.hosny84@gmail.com>
2017-01-30 23:01:10 +0200

    add /crud/drug/create role

--- console/migrations/m170130_181541_addExcelUploadRole.php
+++ console/migrations/m170130_181541_addExcelUploadRole.php
@@ -7,12 +7,13 @@ class m170130_181541_addExcelUploadRole extends Migration
     public function up()
     {
         $this->execute("INSERT INTO auth_item(name,type) VALUES
-      ('/crud/drug/excel-download',2),('/crud/drug/excel-upload',2)");
+      ('/crud/drug/excel-download',2),('/crud/drug/excel-upload',2),('/crud/drug/create',2)");
 
         $this->execute("INSERT INTO auth_item_child(parent,child) VALUES
 
       ('Qppv Person','/crud/drug/excel-upload'),
-      ('Qppv Person','/crud/drug/excel-download')
+      ('Qppv Person','/crud/drug/excel-download'),
+      ('Qppv Person','/crud/drug/create')
       ");
     }
 
@@ -20,8 +21,10 @@ class m170130_181541_addExcelUploadRole extends Migration
     {
         $this->execute('DELETE FROM auth_item_child where parent = "Qppv Person" AND child = "/crud/drug/excel-upload" ');
         $this->execute('DELETE FROM auth_item_child where parent = "Qppv Person" AND child = "/crud/drug/excel-download" ');
+        $this->execute('DELETE FROM auth_item_child where parent = "Qppv Person" AND child = "/crud/drug/create" ');
         $this->execute('DELETE FROM auth_item where name = "/crud/drug/excel-download" AND type = 2 ');
         $this->execute('DELETE FROM auth_item where name = "/crud/drug/excel-upload" AND type = 2 ');
+        $this->execute('DELETE FROM auth_item where name = "/crud/drug/create" AND type = 2 ');
 
     }
 
