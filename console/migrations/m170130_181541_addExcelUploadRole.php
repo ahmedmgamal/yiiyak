51f2e50ac4957b73cf2401f21d77772a40329a96
Mahmooud Hosny <mahmoud.e.hosny84@gmail.com>
2017-01-30 23:38:09 +0200

    remove /crud/drug/create

--- console/migrations/m170130_181541_addExcelUploadRole.php
+++ console/migrations/m170130_181541_addExcelUploadRole.php
@@ -7,7 +7,7 @@ class m170130_181541_addExcelUploadRole extends Migration
     public function up()
     {
         $this->execute("INSERT INTO auth_item(name,type) VALUES
-      ('/crud/drug/excel-download',2),('/crud/drug/excel-upload',2),('/crud/drug/create',2)");
+      ('/crud/drug/excel-download',2),('/crud/drug/excel-upload',2)");
 
         $this->execute("INSERT INTO auth_item_child(parent,child) VALUES
 
@@ -24,7 +24,6 @@ class m170130_181541_addExcelUploadRole extends Migration
         $this->execute('DELETE FROM auth_item_child where parent = "Qppv Person" AND child = "/crud/drug/create" ');
         $this->execute('DELETE FROM auth_item where name = "/crud/drug/excel-download" AND type = 2 ');
         $this->execute('DELETE FROM auth_item where name = "/crud/drug/excel-upload" AND type = 2 ');
-        $this->execute('DELETE FROM auth_item where name = "/crud/drug/create" AND type = 2 ');
 
     }
 
