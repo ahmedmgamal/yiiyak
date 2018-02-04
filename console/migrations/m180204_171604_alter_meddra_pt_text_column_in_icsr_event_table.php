f050e7ccaea5bbde0d66474458e4cf3e35691a6d
Ahmed Doma <ahmedgdoma@gmail.com>
2018-02-04 19:20:05 +0200

    116_auto_complete_icsr_event

---
+++ console/migrations/m180204_171604_alter_meddra_pt_text_column_in_icsr_event_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m180204_171604_alter_meddra_pt_text_column_in_icsr_event_table
+ */
+class m180204_171604_alter_meddra_pt_text_column_in_icsr_event_table extends Migration
+{
+    /**
+     * @inheritdoc
+     */
+//    public function safeUp()
+//    {
+//
+//    }
+//
+//    /**
+//     * @inheritdoc
+//     */
+//    public function safeDown()
+//    {
+//        echo "m180204_171604_alter_meddra_pt_text_column_in_icsr_event_table cannot be reverted.\n";
+//
+//        return false;
+//    }
+
+
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+        $this->alterColumn('icsr_event', 'meddra_pt_text', 'varchar(100)');
+    }
+
+    public function down()
+    {
+        return false;
+    }
+
+}
