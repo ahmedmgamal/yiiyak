ce8e67da2e4427cfec2b7d1fa1afa8599d5775db
ahmedgdoma <ahmedgdoma@gmail.com>
2018-03-22 19:59:44 +0200

    remove forien key

--- console/migrations/m180322_114115_alter_meddra_index.php
+++ console/migrations/m180322_114115_alter_meddra_index.php
@@ -13,8 +13,8 @@ class m180322_114115_alter_meddra_index extends Migration
     // Use up()/down() to run migration code without a transaction.
     public function up()
     {
-        $this->dropIndex('fk_icsr_event_meddra_llt1_idx','icsr_event');
-        $this->dropIndex('fk_icsr_event_meddra_pt1','icsr_event');
+        $this->dropForeignKey('fk_icsr_event_meddra_llt1','icsr_event');
+        $this->dropForeignKey('fk_icsr_event_meddra_pt1','icsr_event');
         $this->alterColumn('icsr_event', 'meddra_llt_id', 'bigint(20)   UNSIGNED');
         $this->alterColumn('icsr_event', 'meddra_pt_id', 'bigint(20)   UNSIGNED');
 
@@ -22,8 +22,7 @@ class m180322_114115_alter_meddra_index extends Migration
 
     public function down()
     {
-        $this->createIndex('fk_icsr_event_meddra_llt1_idx', 'icsr_event', 'meddra_llt_id');
-        $this->createIndex('fk_icsr_event_meddra_llt1_idx', 'icsr_event', 'meddra_pt_id');
+        return false;
     }
 
 }
