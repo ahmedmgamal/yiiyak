e6fb5de73af7de1a44f208858ff2d74e0e0c32dd
Ahmed Doma <ahmedgdoma@gmail.com>
2018-05-25 21:12:16 +0200

    working on testing

---
+++ tests/unit/models/LoginFormTest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace tests\models;
+
+use Yii;
+use tests\codeception\common\unit\DbTestCase;
+use Codeception\Specify;
+use common\models\LoginForm;
+use tests\codeception\common\fixtures\UserFixture;
+
+class LoginFormTest extends \Codeception\Test\Unit
+{
+    private $model;
+
+    protected function _after()
+    {
+        \Yii::$app->user->logout();
+    }
+
+    public function testLoginNoUser()
+    {
+        $this->model = new LoginForm();
+
+        expect_not($this->model->load([
+                'username' => 'not_existing_username',
+                'password' => 'not_existing_password',
+            ]) && $this->model->login());
+        expect_that(\Yii::$app->user->isGuest);
+    }
+
+    public function testLoginWrongPassword()
+    {
+        $this->model = new LoginForm([
+            'username' => 'demo',
+            'password' => 'wrong_password',
+        ]);
+
+        expect_not($this->model->login());
+        expect_that(\Yii::$app->user->isGuest);
+        expect($this->model->errors)->hasKey('password');
+    }
+
+    public function testLoginCorrect()
+    {
+        $this->model = new LoginForm([
+            'username' => 'demo',
+            'password' => 'demo',
+        ]);
+
+        expect_that($this->model->login());
+        expect_not(\Yii::$app->user->isGuest);
+        expect($this->model->errors)->hasntKey('password');
+    }
+
+}
