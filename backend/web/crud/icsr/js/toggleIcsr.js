ccae6589f0f4e18501427c93731c944788c1fa6a
Ahmed Doma <ahmedgdoma@gmail.com>
2018-07-02 15:51:39 +0200

    js

--- backend/web/crud/icsr/js/toggleIcsr.js
+++ backend/web/crud/icsr/js/toggleIcsr.js
@@ -4,12 +4,25 @@ $(document).ready(function(){
         $('.toggleIcsr').toggleClass('btn-success').toggleClass('btn-default');
         $('.toggleIcsrModel').toggle();
     });
-    $(window).on('load', function(){
-        var offset = new Date().getTimezoneOffset();
-        var created_at = $('.toggleIcsrModel table tbody tr td:last-child');
-        created_at.text(
-            new  Date(created_at.text() + offset).toISOString()
-        );
+    var cells = [];
+    function convertUTCDateToLocalDate(date) {
+        var newDate = new Date(date.getTime()+date.getTimezoneOffset()*60*1000);
+
+        var offset = date.getTimezoneOffset() / 60;
+        var hours = date.getHours();
+
+        newDate.setHours(hours - offset);
+
+        return newDate;
+    }
+    $('.toggleIcsrModel table tbody tr  td:last-of-type').each(function() {
+
+        cells.push($(this).html());
+    });
+    $.each(cells , function(index,val) {
+        var date = convertUTCDateToLocalDate(new Date(val));
+        cells[index]= date.toLocaleString();
+        $('.toggleIcsrModel table tbody tr  td:last-of-type').eq( index ).html(cells[index]);
     });
 
 });
